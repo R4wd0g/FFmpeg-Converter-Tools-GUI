6f90e9cd1750916892bf872982587f2a8c585337
R4wd0G <29448208+R4wd0g@users.noreply.github.com>
2026-06-06 19:12:37 -0300

    added: assembly info

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("FFmpeg Converter Tools GUI")]
+[assembly: AssemblyDescription("Native Windows GUI for practical FFmpeg workflows.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("R4wd0G")]
+[assembly: AssemblyProduct("FFmpeg Converter Tools GUI")]
+[assembly: AssemblyCopyright("Copyright (c) 2026 R4wd0G")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
